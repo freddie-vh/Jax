e490303dac20d7d8468ade12bea5cbfc9997f4cf
Freddie <freddie.vernonharcourt@gmail.com>
2026-09-01 14:11:28 +0100

    Ported to jax, removed loops and mutable arrays

--- pendulum.py
+++ pendulum.py
@@ -1,17 +1,19 @@
 import jax.numpy as jnp
+import jax.lax as lax
 
 class Simulator:
     def simulate(self, theta0, omega0, g, L, dt, steps):
-        theta = [theta0]
-        omega = [omega0]
-        for i in range(0, steps):
-            angular_acceleration = -(g/L) * jnp.sin(theta[i])
-            omega.append(omega[i] + dt * angular_acceleration)
-            theta.append(theta[i] + dt * omega[i+1])
-            print(omega[i], theta[i])
-            energy = 0.5 * L**2 * omega[i]**2 - g * L * jnp.cos(theta[i])
-            print(energy)
-        return list(zip(theta, omega))
+        def forward_step(carry, x):
+            theta, omega = carry
+            angular_acceleration = -(g/L) * jnp.sin(theta)
+            new_omega = omega + dt * angular_acceleration
+            new_theta = theta + dt * new_omega
+            return jnp.array([new_theta, new_omega]), jnp.array([new_theta, new_omega])
+
+        final, result = lax.scan(forward_step, init=jnp.array([theta0, omega0]), xs=None, length=steps)
+        initial = jnp.array([theta0, omega0])
+        trajectories = jnp.concatenate([initial[None, :], result], axis=0)
+        return trajectories
 
 mySimulator = Simulator()
-mySimulator.simulate(0.5, 0.0, 9.81, 1.0, 0.01, 100)
+print(mySimulator.simulate(0.5, 0.0, 9.81, 1.0, 0.01, 10))
